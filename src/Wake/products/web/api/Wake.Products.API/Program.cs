263e8448a6fc60f0190b7f38cf5b7d7e1b3ff2c9
Bruno Bastiani <brunobastiani95@gmail.com>
2024-03-08 13:21:01 -0300

    config: Adiciona Estrutura Inicial ao Projeto

---
+++ src/Wake/products/web/api/Wake.Products.API/Program.cs
@@ -0,0 +1,27 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
